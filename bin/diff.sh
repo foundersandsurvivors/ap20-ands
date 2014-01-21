08cf476c49077992b35bb6eb78d956deb8fd86d3
Sandra Silcot <ssilcot@ldap.faspriv>
2014-01-21 18:10:34 +1100

    Diff.sh to use AP20_WEBPATH not AP20_APP (legacy/devfas)

--- bin/diff.sh
+++ bin/diff.sh
@@ -3,13 +3,13 @@
 OUT="/tmp/diff.log"
 OUT2="/tmp/diff-summary.log"
 DISTRO="../src/www/ap20/yggdrasil"
-if [[ "$AP20_APP" == "" ]]; then
-    echo "$0 requires AP20_APP environment to be set (location of deployed distro)"
+if [[ "$AP20_WEBPATH" == "" ]]; then
+    echo "$0 requires AP20_WEBPATH environment to be set (location of your deployed code distro)"
     exit 0
 fi
 echo "##==== $0: Whole dir diff for Yggdrasil webapp"
-echo "-- from distro[$DISTRO]"
-echo "-- to deployed[$AP20_APP]"
+echo "-- from   distro[$DISTRO]"
+echo "-- to   deployed[$AP20_WEBPATH]"
 diff -Naur -X rsync.phpapp.dep2repo.exclusions ../src/www/ap20/yggdrasil $AP20_APP > $OUT 2>&1
 grep '^--- ' $OUT > $OUT2
 ls -la $OUT $OUT2
