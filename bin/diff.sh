84c2ca0ce242e82645e44a1bf5e938f43591f9d2
Sandra Silcot <ssilcot@ap20y1.nectar>
2015-08-14 16:37:52 +1000

    bin/diff.sh modded comments and exclusions

--- bin/diff.sh
+++ bin/diff.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# $AP20_HOME/bin/diff.sh : shows all code changes between the distro dir (here)
+#                          and the deployed web application ($AP20_WEBPATH)
+
+# run from the dir this script is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 OUT="/tmp/diff.log"
 OUT2="/tmp/diff-summary.log"
 DISTRO="../src/www/ap20/yggdrasil"
@@ -8,8 +15,8 @@ if [[ "$AP20_WEBPATH" == "" ]]; then
     exit 0
 fi
 echo "##==== $0: Whole dir diff for Yggdrasil webapp"
-echo "-- from   distro[$DISTRO]"
-echo "-- to   deployed[$AP20_WEBPATH/]"
-diff -Naur -X rsync.phpapp.dep2repo.exclusions ../src/www/ap20/yggdrasil $AP20_WEBPATH > $OUT 2>&1
-grep '^--- ' $OUT > $OUT2
+echo "-- from deployed[$AP20_WEBPATH/]"
+echo "-- to   distro[$DISTRO]"
+diff -Naur -X rsync.phpapp.dep2repo.exclusions $AP20_WEBPATH ../src/www/ap20/yggdrasil > $OUT 2>&1
+agrep '^--- ,^\+\+\+' $OUT > $OUT2
 ls -la $OUT $OUT2
